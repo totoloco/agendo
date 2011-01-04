7d07357ba589537621974887db4dcd4f7cd0c894
ushcompu <ushcompu@ushcompu.com.ar>
2011-01-04 20:11:12 -0300

    agregando el modelo de materias, simple, casi sin nada u.u

---
+++ alumnos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# subject == materia
+class Subject(models.model):
+  name = models.CharField(max_length=255)
+  teachers = models.CharField(max_length=255)
+  teacher_emails = models.CharField(max_length=255)
+  url = models.CharField(max_length=255)
+  # opcionales para agregar: is_active, created, updated, user_id (el usuario que creó la materia)
