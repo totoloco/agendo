1215c6da12e46f187456e014e5548256a3742645
ushcompu <ushcompu@ushcompu.com.ar>
2011-01-06 13:04:28 -0300

    borrando el lio que hizo The Hummit, poniendo 4 espacios como TABs, y el_lobo--d-_-b es emo orejón

--- alumnos/models.py
+++ alumnos/models.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # subject == materia
 class Subject(models.Model):
-  name = models.CharField(max_length=255)
-  teachers = models.CharField(max_length=255)
-  teacher_emails = models.CharField(max_length=255)
-  url = models.CharField(max_length=255)
-  # opcionales para agregar: is_active, created, updated, user_id (el usuario que creO la materia)
+    name = models.CharField(max_length=255)
+    teachers = models.CharField(max_length=255)
+    teacher_emails = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    # opcionales para agregar: is_active, created, updated, user_id (el usuario que creó la materia)
 
-  def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+        return self.name
--- alumnos/views.py
+++ alumnos/views.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.template import Context, loader
 from alumnos.models import Subject
 from django.http import HttpResponse
 
+# la creación de materias por ahora sólo se realiza desde el admin
+
 def index(request):
-  subject_list = Subject.objects.all().order_by('name')#[:10]
-  t = loader.get_template('subject/index.html')
-  c = Context({
-    'subject_list': subject_list,
-  })
-  return HttpResponse(t.render(c))
+    subject_list = Subject.objects.all().order_by('name')#[:10]
+    t = loader.get_template('subject/index.html')
+    c = Context({
+        'subject_list': subject_list,
+    })
+    return HttpResponse(t.render(c))
