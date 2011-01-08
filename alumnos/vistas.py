f5dce64581094b425baa772e3a7c996164042922
totoloco ushcompu <totoloco@ushcompu.com.ar>
2011-01-08 20:49:11 -0300

    finalmente tenemos contexto para saber si un usuario está logueado o no :D

--- alumnos/vistas.py
+++ alumnos/vistas.py
@@ -7,7 +7,12 @@ from django.template import RequestContext
 # La creación de materias por ahora sólo se realiza desde la interfaz
 # de administración.
 
+def render_response(peticion, *args, **kwargs):
+    kwargs['context_instance'] = RequestContext(peticion)
+    return render_to_response(*args, **kwargs)
+
 def lista(peticion):
     lista_materias = Materia.objects.all().order_by('nombre')#[:10]
-    return render_to_response('materia/lista.html',
-                              {'lista_materias': lista_materias})
+    return render_response(peticion,
+                           'materia/lista.html',
+                           {'lista_materias': lista_materias})
